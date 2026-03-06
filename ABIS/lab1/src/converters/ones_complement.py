4108605292fc0c72b35f569e8755fc16bbb911b7
hedgehog-os <explayeroha@example.com>
2026-03-07 01:33:30 +0300

    Implement ones complement

--- ABIS/lab1/src/converters/ones_complement.py
+++ ABIS/lab1/src/converters/ones_complement.py
@@ -1,5 +1,5 @@
 from src.core.bit_array32 import BitArray32
-from src.converters.decimal_binary import DecimalBinaryConverter
+from src.converters.sign_magnitude import SignMagnitudeCodec
 from src.core.interfaces import IntegerCodec
 
 
@@ -7,8 +7,30 @@ class OnesComplementCodec(IntegerCodec):
     """Ones' complement integer representation codec."""
 
     def encode(self, value: int) -> BitArray32:
-        pass
+        converter = SignMagnitudeCodec()
 
+        if value >= 0:
+            bits = converter.encode(value)
+            return bits
+        
+        else:
+            bits = converter.encode(abs(value))
+            for i in range(bits.SIZE):
+                bits[i] = 1 - bits[i]
 
-    def decode(self, bits: BitArray32) -> int:
-        pass
+            return bits
+
+    def decode(self, input_bits: BitArray32) -> int:
+        converter = SignMagnitudeCodec()
+        bits = input_bits.copy()
+        
+        if bits.bits[0] == 0:
+            value = converter.decode(bits)
+            return value
+        
+        else:
+            for i in range(1, bits.SIZE):
+                bits[i] = 1 - bits[i]
+
+            value = converter.decode(bits)
+            return value
